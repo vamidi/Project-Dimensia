0b50ac5ed2b4c955a80c966e886b126bdb4bbc22
vamidi <valencio_masaki16@live.nl>
2015-12-13 22:05:12 +0100

    Project build added
    This is my main project build for the prototype.
    Please as non-programmer do not touch this build.
    Designers will get a test build that runs async with the current build.

---
+++ Programmers/prototype/Assets/Scripts/Camera2D.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+*   
+*   Camera2D.cs 
+*   
+*/
+
+public class Camera2D : MonoBehaviour {
+
+    //
+    public Transform player;
+
+    public Vector2 mMargin, mSmoothing;
+
+    public BoxCollider2D mBounds;
+
+    public Vector3 mMin, mMax;
+
+    public bool isFollowing { get; set; }
+
+	// Use this for initialization
+	void Start () {
+        mMin = mBounds.bounds.min;
+        mMax = mBounds.bounds.max;
+        isFollowing = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var x = transform.position.x;
+        var y = transform.position.y;
+
+        if (isFollowing) {
+            if(Mathf.Abs(x - player.transform.position.x) > mMargin.x)
+            {
+                x = Mathf.Lerp(x, transform.position.x, mSmoothing.x * Time.deltaTime);
+            }
+
+            if(Mathf.Abs(y - player.transform.position.y) > mMargin.y)
+            {
+                y = Mathf.Lerp(y, transform.position.y, mSmoothing.y * Time.deltaTime);
+            }
+
+            var cameraHalfWidth = Camera.main.orthographicSize * ((float)Screen.width / Screen.height);
+            x = Mathf.Clamp(x, mMin.x + cameraHalfWidth, mMax.x - cameraHalfWidth);
+            y = Mathf.Clamp(y, mMin.y + Camera.main.orthographicSize, mMax.y - cameraHalfWidth);
+
+            transform.position = new Vector3(x, y, transform.position.z);
+
+        }
+    }
+}
---
+++ Programmers/prototype/Assets/simpleMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class simpleMovement : MonoBehaviour {
+
+    public Rigidbody mRigidBody;
+
+    public float speeds = 5f;
+
+	// Use this for initialization
+	void Start () {
+        if (this.mRigidBody == null)
+            this.mRigidBody = this.GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.D))
+            this.mRigidBody.velocity += new Vector3(1 + speeds, 0) * Time.deltaTime;
+        if(Input.GetKeyDown(KeyCode.A))
+            this.mRigidBody.velocity -= new Vector3(1 + speeds, 0) * Time.deltaTime;
+
+        this.mRigidBody.velocity = Vector2.zero;
+
+    }
+}
